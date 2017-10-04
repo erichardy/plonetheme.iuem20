d0525eb6f548a4ff7adc2af6ff61ecfb85bfd786
Eric Hardy <Eric.Hardy@univ-brest.fr>
2017-10-04 12:38:54 +0200

    add viewlet share with fixed position

---
+++ src/plonetheme/iuem20/browser/viewlets/iuem20_share.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+from plone.app.layout.viewlets import common as base
+
+import logging
+
+
+# from plonetheme.iuem20.utils import getHomeObject
+# from plonetheme.iuem20.utils import getSettingValue
+
+logger = logging.getLogger('iuem20-share')
+
+
+class iuem20Share(base.ViewletBase):
+
+    pass
